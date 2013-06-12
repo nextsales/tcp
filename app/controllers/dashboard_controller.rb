a03a6415ab812e3a0eb493ec7a9cdf2de09279da
Quang Pham <test@quangpham.com>
2013-06-12 18:05:58 +0300

    add more example

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,14 +7,21 @@ class DashboardController < ApplicationController
   end
   
   def testlinkedin
+    
+    #raise get_all_updates_by_keyword("digital").to_json
+    
+    raise search_company("digital").to_yaml
+    
+    
     #raise current_user.linkedin_client.profile(:url => 'http://www.linkedin.com/pub/pham-quang/50/316/72').to_yaml
     #raise get_company_data(79870).to_yaml
     #raise get_company_update(79870,"UNIU-c79870-5748859230270525440-FOLLOW_CMPY").to_yaml
-    #raise get_company_updates(79870).to_yaml
+    raise get_company_updates(79870).to_yaml
     
     @update = []
     @update.push(get_company_update(79870,"UNIU-c79870-5748859230270525440-FOLLOW_CMPY"));
     @update.push(get_company_update(79870,"UNIU-c79870-5739788179427307520-FOLLOW_CMPY"));
+    
     raise @update.to_yaml
   end
   
@@ -38,4 +45,14 @@ class DashboardController < ApplicationController
     #current_user.linkedin_client.search({ :keywords => keyword}, "company").companies.all
     current_user.linkedin_client.search({:keywords => keyword}, "company")
   end
+  
+  def get_all_updates_by_keyword(keyword)
+    company_updates = []
+    search_company(keyword).companies.all.each do |company|
+      company_updates += get_company_updates(company.id).all if (get_company_updates(company.id).all)
+    end
+    company_updates
+  end
+  
+  
 end
