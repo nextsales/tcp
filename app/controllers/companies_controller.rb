586ca4625b0599e0bef255701d7f4bcb9e01bbb9
Quang Pham <me@quangpham.com>
2013-06-17 06:41:46 +0300

    revert

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,7 +4,7 @@ class CompaniesController < ApplicationController
   # GET /companies
   # GET /companies.json
   def index
-    @companies = current_user.companies
+    @companies = Company.all
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @companies }
