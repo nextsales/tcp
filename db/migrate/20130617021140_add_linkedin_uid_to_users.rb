b4b18532002232ece9f2db4b5460e4fefc85421f
Quang Pham <me@quangpham.com>
2013-06-17 05:40:00 +0300

    add index linkedin_uid to Users

--- db/migrate/20130617021140_add_linkedin_uid_to_users.rb
+++ db/migrate/20130617021140_add_linkedin_uid_to_users.rb
@@ -1,6 +1,6 @@
 class AddLinkedinUidToUsers < ActiveRecord::Migration
   def change
     add_column :users, :linkedin_uid, :string
+    add_index :users, :linkedin_uid, :unique => true
   end
-  add_index :users, :linkedin_uid, :unique => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -279,6 +279,7 @@ ActiveRecord::Schema.define(:version => 20130617021140) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["linkedin_uid"], :name => "index_users_on_linkedin_uid", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "users_roles", :id => false, :force => true do |t|
