0a846c05843b9f89e7a5435cd0240adfe64748df
Quang Pham <test@quangpham.com>
2013-06-20 15:31:08 +0300

    add is_enable into suggested_companies

--- app/models/suggested_company.rb
+++ app/models/suggested_company.rb
@@ -1,4 +1,4 @@
 class SuggestedCompany < ActiveRecord::Base
-  attr_accessible :country, :linkedin_id, :logo_url, :name, :rank, :raw_data, :user_id
+  attr_accessible :country, :linkedin_id, :logo_url, :name, :rank, :raw_data, :user_id, :is_enable
   belongs_to :user
 end
