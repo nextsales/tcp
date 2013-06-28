1efdbfc357b39ee27fc746277ca88b363de34a7d
Quang Pham <phama@quangpham.com>
2013-06-28 13:28:31 +0300

    Use coffee script

--- app/assets/javascripts/select2.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function($){
-	$("#company_industry_ids").select2();
-	$("#company_competence_ids").select2();
-	$("#company_matrix_ids").select2();
-});
---
+++ app/assets/javascripts/select2.js.coffee
@@ -0,0 +1,4 @@
+jQuery ->
+	$("#company_industry_ids").select2()
+	$("#company_competence_ids").select2()
+	$("#company_matrix_ids").select2()
