e1cc06e9a1090ef46bc4980931eb6764ec6f2558
Quang Pham <phama@quangpham.com>
2013-06-28 12:59:06 +0300

    refactor

---
+++ app/assets/javascripts/activemenu.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('ul#side-nav li a').each(function(){
+		if (String(window.location).indexOf($($(this))[0].href) >= 0)
+			$(this).parent().addClass('active');
+	});
+});
---
+++ app/assets/javascripts/select2.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($){
+	$("#company_industry_ids").select2();
+	$("#company_competence_ids").select2();
+	$("#company_matrix_ids").select2();
+});
