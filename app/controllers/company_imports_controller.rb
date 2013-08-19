58ac8f876e0721515a6b5b31a6388660843edc2b
Quang Pham <me@quangpham.com>
2013-08-19 19:58:43 +0300

    quick fix

--- app/controllers/company_imports_controller.rb
+++ app/controllers/company_imports_controller.rb
@@ -9,26 +9,27 @@ class CompanyImportsController < ApplicationController
     @company_import.current_user=current_user
     if @company_import.save
       redirect_to root_url, notice: "Imported companies successfully."
-    end
-    #else
-      #render :new
     #end
-    case File.extname(@company_import.file.original_filename)
-      when ".csv" then spreadsheet = Roo::Csv.new(@company_import.file.path, nil, :ignore)
-      when ".xls" then spreadsheet = Roo::Excel.new(@company_import.file.path, nil, :ignore)
-      when ".xlsx" then spreadsheet = Roo::Excelx.new(@company_import.file.path, nil, :ignore)
-      else raise "Unknown file type: #{@company_import.file.original_filename}"
+    else
+      render :new
     end
     
-    @imported_companies =Array.new
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).map do |i|
-      row = Hash[[header, spreadsheet.row(i)].transpose]
-      company = Company.find_by_id(row["id"]) || Company.new
-      company.attributes = row.to_hash.slice(*Company.accessible_attributes)
-      @imported_companies << company      
-    end
-    
-    render "preview.html.erb"
+    # case File.extname(@company_import.file.original_filename)
+    #   when ".csv" then spreadsheet = Roo::Csv.new(@company_import.file.path, nil, :ignore)
+    #   when ".xls" then spreadsheet = Roo::Excel.new(@company_import.file.path, nil, :ignore)
+    #   when ".xlsx" then spreadsheet = Roo::Excelx.new(@company_import.file.path, nil, :ignore)
+    #   else raise "Unknown file type: #{@company_import.file.original_filename}"
+    # end
+    # 
+    # @imported_companies =Array.new
+    # header = spreadsheet.row(1)
+    # (2..spreadsheet.last_row).map do |i|
+    #   row = Hash[[header, spreadsheet.row(i)].transpose]
+    #   company = Company.find_by_id(row["id"]) || Company.new
+    #   company.attributes = row.to_hash.slice(*Company.accessible_attributes)
+    #   @imported_companies << company      
+    # end
+    # 
+    # render "preview.html.erb"
   end
 end
