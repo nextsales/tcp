17960e5ac85aa33e249987a45a2162e3da6610ac
Quang Pham <test@quangpham.com>
2013-09-19 09:58:42 +0300

    .

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -159,3 +159,74 @@ $(function(){
     });
 
 });
+
+function testData(stream_names, points_count) {
+    var now = new Date().getTime(),
+        day = 1000 * 60 * 60 * 24, //milliseconds
+        days_ago_count = 60,
+        days_ago = days_ago_count * day,
+        days_ago_date = now - days_ago,
+        points_count = points_count || 45, //less for better performance
+        day_per_point = days_ago_count / points_count;
+    return stream_layers(stream_names.length, points_count, .1).map(function(data, i) {
+        return {
+            key: stream_names[i],
+            values: data.map(function(d,j){
+                return {
+                    x: days_ago_date + d.x * day * day_per_point,
+                    y: Math.floor(d.y * 100) //just a coefficient
+                }
+            })
+        };
+    });
+}
+
+
+var lineResize,
+    lineChart;
+function lineChartOperaHack(){
+    //lineChart is somehow not rendered correctly after updates. Need to reupdate
+    
+/*if ($.browser.opera){
+        clearTimeout(lineResize);
+        lineResize = setTimeout(lineChart.update, 300);
+    }
+*/
+}
+nv.addGraph(function() {
+    var chart = nv.models.lineChart()
+        .margin({top: 0, bottom: 25, left: 25, right: 0})
+        //.showLegend(false)
+        .color([
+            //$orange, '#cf6d51'
+            '#618fb0', '#61b082'
+        ]);
+
+    chart.legend.margin({top: 3});
+
+    chart.yAxis
+        .showMaxMin(false)
+        .tickFormat(d3.format(',.f'));
+
+    chart.xAxis
+        .showMaxMin(false)
+        .tickFormat(function(d) { return d3.time.format('%b %d')(new Date(d)) });
+    var data = testData(['Unique', 'Visits'], 30);
+    data[0].area = true;
+    d3.select('#visits-chart svg')
+        .datum(data)
+        .transition().duration(500)
+        .call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    chart.legend.dispatch.on('legendClick.updateExamples', function() {
+        lineChartOperaHack();
+    });
+
+    lineChart = chart;
+
+    lineChartOperaHack();
+
+    return chart;
+});
